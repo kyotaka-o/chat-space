735a6040257bab22aef268c5a322ef584fd1e100
kyotaka-o <kyo-taka@docomo.ne.jp>
2019-07-07 22:23:19 +0900

    change
    重複部分の修正および、グループメンバー送信部の修正

--- app/assets/javascripts/serach.js
+++ app/assets/javascripts/serach.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function() { 
   var search_list = $("#user-search-result");
-  function appendProduct(user) {
+
+  function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
                 <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
@@ -29,9 +30,10 @@ $(document).on('turbolinks:load', function() {
     })
     .done(function(users) {
       search_list.empty();
+      
       if (users.length !== 0) {
         users.forEach(function(user){
-          appendProduct(user);
+          appendUser(user);
         });
       }
       else {
@@ -43,43 +45,18 @@ $(document).on('turbolinks:load', function() {
     })
   });
 
-  $("#serach-input").on("keyup", function() {
-    var input = $("#serach-input").val();
-    if (input === ""){
-      search_list.empty();
-      return;
-    }
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(users) {
-      search_list.empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendProduct(user);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致するユーザーが見つかりません");
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    })
-  });
   $(document).on("click", ".chat-group-user__btn--add", function () {
     var name =$(this).attr('data-user-name');
     var id =$(this).attr('data-user-id');
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                 <input name='group[user_ids][]' type='hidden' value='${id}'>
                 <p class='chat-group-user__name'>${name}</p>
                 <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`;
     $(".chat-group-users").append(html);
+    $(this).parent().remove();
   });
+
   $(document).on("click", ".js-remove-btn", function () {
     $(this).parent().remove();
   });
